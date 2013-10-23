da4c768b5f3160027aa5695891a82fac0c22fad4
Hackbright Student <user@mini-004.(none)>
2013-10-23 15:29:59 -0700

    Cleaned up code and put things into neat functions

--- client.py
+++ client.py
@@ -1,13 +1,9 @@
 import socket, sys, select
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_socket.connect(("localhost", 5555)) #10.1.10.100
-
-def main():
-    pass
-
 def open_connection(host, port):
-    pass
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    my_socket.connect((host, port)) #10.1.10.100
+    return my_socket
 
 def format_message(msg):
     if "::" in msg:
@@ -16,25 +12,29 @@ def format_message(msg):
     else:
         return msg 
 
+def main():
+    my_socket = open_connection("10.1.10.100", 5555)
 
-running = True
-while running:
-    inputready, outputready, exceptready = select.select([my_socket, sys.stdin], [], [])
+    running = True
+    while running:
+        inputready, outputready, exceptready = select.select([my_socket, sys.stdin], [], [])
 
-    for s in inputready:
-        if s == my_socket:
-            msg = s.recv(1024)
-            if msg:
-                print format_message(msg)
-            else:
-                print "Disconnected from server!"
-                running = False
-        elif s == sys.stdin:
-            user_input = s.readline().rstrip("\n")
+        for s in inputready:
+            if s == my_socket:
+                msg = s.recv(1024)
+                if msg:
+                    print format_message(msg)
+                else:
+                    print "Disconnected from server!"
+                    running = False
+            elif s == sys.stdin:
+                user_input = s.readline().rstrip("\n")
 
-            if user_input == "quit":
-                running = False
-            else:
-                my_socket.sendall(user_input)
+                if user_input == "quit":
+                    running = False
+                else:
+                    my_socket.sendall(user_input)
 
-my_socket.close()
+    my_socket.close()
+
+main()
