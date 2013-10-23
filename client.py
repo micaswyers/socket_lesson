edaacd7a1637cb4b0dd69a199f883539403e7696
Hackbright Student <user@mini-004.(none)>
2013-10-23 15:25:55 -0700

    Formatted messages when there is a username

--- client.py
+++ client.py
@@ -1,7 +1,20 @@
 import socket, sys, select
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_socket.connect(("localhost", 5555))
+my_socket.connect(("localhost", 5555)) #10.1.10.100
+
+def main():
+    pass
+
+def open_connection(host, port):
+    pass
+
+def format_message(msg):
+    if "::" in msg:
+        message = msg.split("::", 1)
+        return "[%s] %s" % (message[0], message[1])
+    else:
+        return msg 
 
 
 running = True
@@ -11,9 +24,8 @@ while running:
     for s in inputready:
         if s == my_socket:
             msg = s.recv(1024)
-
             if msg:
-                print msg
+                print format_message(msg)
             else:
                 print "Disconnected from server!"
                 running = False
